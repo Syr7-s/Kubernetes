5d706d216c36d07ecff3a9f1bc124ef28c6281ff
İsa SAYAR <isa.sayar1725@gmail.com>
2022-04-04 23:50:00 +0300

    Initial commit.

---
+++ Kubernetes For Java Developers/JavaExInKubernetes/src/main/java/com/syrisa/javaexinkubernetes/JavaExInKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.javaexinkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExInKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaExInKubernetesApplication.class, args);
+    }
+
+}
---
+++ Kubernetes For Java Developers/JavaExInKubernetes/src/test/java/com/syrisa/javaexinkubernetes/JavaExInKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.javaexinkubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExInKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
